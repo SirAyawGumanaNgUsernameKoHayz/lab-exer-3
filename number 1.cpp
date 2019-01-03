9cff78826e8f643bdd1306295c428566d2b132d3
SirAyawGumanaNgUsernameKoHayz <46342364+SirAyawGumanaNgUsernameKoHayz@users.noreply.github.com>
2019-01-03 18:44:30 +0800

    Add files via upload

---
+++ number 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+
+//.1
+int main()
+{
+    char B;
+    cout << "Enter a character: ";
+    cin >> B;
+    for(int i = 0; i < 10; i++) {
+     cout << B << " ";
+    }
+    return 0;
+}
+
+
---
+++ number 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+//.2
+int main()
+{
+	char A;
+	cout << "enter a Character: ";
+	cin >> A;
+	cout << "ASCII Value of" << A << "is" << int (A);
+
+	return 0;
+}
+
+
